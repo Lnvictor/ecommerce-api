643286b4233fe66951f1fcc5b2a8e22e170884f2
Lnvictor <Lnvictor@github.com>
2021-03-05 18:48:50 -0300

    ipdb remove

--- ecommerceapi/core/facade.py
+++ ecommerceapi/core/facade.py
@@ -8,6 +8,6 @@ def get_products_from_domain(domain: int):
     """
     Get all products for a given domain
     """
-    import ipdb;ipdb.sset_trace()
+
     domain = Domain.objects.filter(pk=domain).get()
     return Product.objects.filter(domain=domain).all()
